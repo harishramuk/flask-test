278f0d430f222d232ba41cdbdb8ce25295ee0d49
harishramuk <50021086+harishramuk@users.noreply.github.com>
2021-03-24 12:25:17 +0530

    h1 tag

--- firstflask.py
+++ firstflask.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def helloIndex():
-    return 'Hello World from Python Flask! this is harish '
+    return '<h1>Hello World from Python Flask! this is harish </h1>'
 
 app.run(host='0.0.0.0', port=5000)
